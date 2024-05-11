3c11a7385de5c3382f3df1c9aebd94fcfde3dadb
EduardoGar0710 <162521123+EduardoGar0710@users.noreply.github.com>
2024-05-11 16:23:56 +0000

    Avance 1

---
+++ src/tui_test.cpp
@@ -0,0 +1,86 @@
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/color.hpp>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <experimental/random>
+#include <list>
+#include <fstream>
+
+using namespace std;
+using namespace ftxui;
+
+int main(int argc, char const *argv[])
+{
+ list <string> textos ;
+ fstream imagen;
+ imagen.open("./assets/imagen.txt");
+
+ string linea;
+ 
+ while (getline(imagen,linea)){
+    textos.push_back(linea);
+ }
+ imagen.close();
+ int fotograma =0;   
+ string reset;
+ 
+ int posX =0;
+ int posy =6;
+
+ while (true)
+ {
+    fotograma++;
+    
+    int r= std::experimental::randint (0,255);
+    int g= std::experimental::randint (0,255);
+    int b= std::experimental::randint (0,255);
+    
+    Element personaje = spinner (21,fotograma);
+    Decorator colorFondo= bgcolor (Color::RGB(r,g,b));
+    Decorator colorTexto = color (Color::RGB (b,g,r));
+    Element dibujo =  border ({
+        hbox(personaje) 
+    }) |colorFondo | colorTexto;
+    
+    Dimensions Alto = Dimension::Fixed (350);
+    Dimensions Ancho = Dimension::Full ();
+
+
+    Screen pantalla = Screen::Create(Ancho,Alto);
+   
+    Render (pantalla,dibujo);
+   
+   int l=0;
+    
+
+    for (auto &&texto : textos)
+    {
+     
+           int i=0;
+    for (auto &&letra : texto)
+    {
+         pantalla.PixelAt(posX + i ,posy + l).character =letra;
+         i ++;
+    }
+    l++;
+
+
+    }
+    
+   posX++;
+
+    pantalla.Print();
+    reset= pantalla.ResetPosition();
+    cout<<reset;
+    this_thread::sleep_for(0.1s);
+
+ }
+ 
+   
+
+
+
+    return 0;
+}
